ecf7e0fa19814341275747c0d341338957615679
aryn <aryndevelop@gmail.com>
2023-03-10 00:12:09 +0530

    Handling breaking news response

--- app/src/main/java/com/example/globalnews/ui/NewsViewModel.kt
+++ app/src/main/java/com/example/globalnews/ui/NewsViewModel.kt
@@ -1,9 +1,37 @@
 package com.example.globalnews.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.globalnews.models.NewsResponse
 import com.example.globalnews.respository.NewsRepository
+import com.example.globalnews.util.Resource
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class NewsViewModel (
-    val newsRepository: NewsRepository
+    private val newsRepository: NewsRepository
 ) : ViewModel() {
+
+    val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    private val breakingNewsPage = 1
+
+    init {
+        getBreakingNews("in")
+    }
+
+    private fun getBreakingNews(countryCode: String) = viewModelScope.launch {
+        breakingNews.postValue(Resource.Loading())
+        val response = newsRepository.getBreakingNews(countryCode, breakingNewsPage)
+        breakingNews.postValue(handleBreakingNewsResponse(response))
+    }
+
+    private fun handleBreakingNewsResponse(response: Response<NewsResponse>) : Resource<NewsResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let { newsResponse ->
+                return Resource.Success(newsResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
 }
--- app/src/main/java/com/example/globalnews/ui/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/example/globalnews/ui/fragments/BreakingNewsFragment.kt
@@ -1,14 +1,19 @@
 package com.example.globalnews.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.globalnews.R
+import com.example.globalnews.adapters.NewsAdapter
 import com.example.globalnews.databinding.FragmentBreakingNewsBinding
 import com.example.globalnews.ui.NewsActivity
 import com.example.globalnews.ui.NewsViewModel
+import com.example.globalnews.util.Resource
 
 class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
 
@@ -17,12 +22,52 @@ class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
 
     lateinit var viewModel: NewsViewModel
 
+    lateinit var newsAdapter: NewsAdapter
+
+    private val TAG = "BreakingNewsFragment"
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel = (activity as NewsActivity).viewModel
+        setupRecyclerView()
+
+        viewModel.breakingNews.observe(viewLifecycleOwner, Observer { response ->
+            when (response){
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.let { newsResponse ->
+                        newsAdapter.differ.submitList(newsResponse.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    hideProgressBar()
+                    response.message?.let { message ->
+                        Log.e(TAG, "An error occured: $message")
+                    }
+                }
+                is Resource.Loading -> {
+                    showProgressBar()
+                }
+            }
+        })
     }
 
+    private fun hideProgressBar() {
+        binding.paginationProgressBar.visibility = View.INVISIBLE
+    }
+
+    private fun showProgressBar() {
+        binding.paginationProgressBar.visibility = View.VISIBLE
+    }
+
+    private fun setupRecyclerView() {
+        newsAdapter = NewsAdapter()
+        binding.rvBreakingNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
